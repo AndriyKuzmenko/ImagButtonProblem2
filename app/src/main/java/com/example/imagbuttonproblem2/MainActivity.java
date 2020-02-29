777665e868657e6efbbc25a4555f4e716002d473
Andriy <ak8022@bs.amalnet.k12.il>
2020-02-29 18:27:37 +0200

    Final push

--- app/src/main/java/com/example/imagbuttonproblem2/MainActivity.java
+++ app/src/main/java/com/example/imagbuttonproblem2/MainActivity.java
@@ -1,14 +1,42 @@
 package com.example.imagbuttonproblem2;
 
 import androidx.appcompat.app.AppCompatActivity;
-
+import java.util.Random;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity
+{
+    ImageView myImageView;
+    Random myRandom;
+    int x;
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState)
+    {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        myImageView=(ImageView)findViewById(R.id.imageView);
+        myRandom=new Random();
+    }
+
+    public void buttonPressed(View view)
+    {
+        x=myRandom.nextInt(3)+1;
+
+        switch(x)
+        {
+            case 1:
+                myImageView.setImageResource(R.drawable.londoneye);
+                break;
+            case 2:
+                myImageView.setImageResource(R.drawable.wullie);
+                break;
+            case 3:
+                myImageView.setImageResource(R.drawable.niagara);
+                break;
+        }
     }
 }
